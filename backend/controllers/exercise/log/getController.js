e54fdecdcbfdb545db20f44b89ab4ca2194e03c8
OrangeSpacer <maxaev.art@yandex.ru>
2022-09-30 21:06:40 +0300

    feat: add script for get exercises log

--- backend/controllers/exercise/log/getController.js
+++ backend/controllers/exercise/log/getController.js
@@ -2,7 +2,7 @@ import asyncHandler from 'express-async-handler'
 import { reBuildTimes } from '../../../helper/exerciseLog.js'
 import ExerciseLog from '../../../models/exerciseLogModel.js'
 
-// @desc Get new exercises
+// @desc Get new exerciseLog
 // @route Get /api/exercises/log/:id
 // @access Private
 
@@ -34,4 +34,26 @@ export const getExerciseLog = asyncHandler(async (req,res) => {
         ...exerciseLog,
         times: newTimes
     })
+})
+
+// @desc Get logs of exercise
+// @route GET /api/exercises/log
+// @access Private
+
+export const getExerciseLogList = asyncHandler(async (req,res) => {
+    const exerciseLogs = await ExerciseLog.find({
+        user: req.user._id,
+        completed: true
+    })
+    .populate('exercise', 'name imageName')
+    .select('exercise createdAt')
+    .lean()
+
+    if(!exerciseLogs){
+        res.status(404)
+        throw new Error('Логи не найдены')
+    }
+    else{
+        res.json(exerciseLogs)
+    }
 })
