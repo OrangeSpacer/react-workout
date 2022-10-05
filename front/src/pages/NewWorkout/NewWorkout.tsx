8de351a98a0fda32938627213344e12bcedc2bcf
OrangeSpacer <maxaev.art@yandex.ru>
2022-10-05 13:53:10 +0300

    feat: add form for create new workout

--- front/src/pages/NewWorkout/NewWorkout.tsx
+++ front/src/pages/NewWorkout/NewWorkout.tsx
@@ -1,22 +1,55 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Field from '../../components/Field/Field'
 import Title from '../../components/Title/Title'
 import Button from '../../components/UI/Button/Button'
 import Select from 'react-select'
 import './NewWorkout.scss'
+import { useMutation, useQuery } from 'react-query'
+import { $api } from '../../components/api/api'
+import { useAuth } from '../../hooks/useAuth'
+import Alert from '../../components/Alert/Alert'
 
 const NewWorkout = ():JSX.Element => {
+  const {isAuth} = useAuth()
   const [name,setName] = useState('')
-  const [exercises,setExercises] = useState()
+  const [exercisesId,setExercisesId]:any = useState([])
+  const [currentExercise,setCurrentExercise]:any= useState([])
 
-  const options: {value:string,label:string}[] = [
-    { value: 'ertehg56gre', label: 'Push-ups' },
-    { value: 'trtfeg1123grg', label: 'Pull-ups' },
-  ]
+  const {isSuccess} = useQuery('Get exercise', () => $api({
+    url:'/exercises',
+    type: "GET"
+  }
+  ),{
+    onSuccess(data){
+      setExercisesId(data)
+    },
+    enabled: isAuth,
+    refetchOnWindowFocus: false
+  })
 
+  const {mutate,error, isSuccess: postSuccess} = useMutation('Create new Workout', ({exIds}:any) => $api({
+    url:'/workouts',
+    type: "POST",
+    body: {name,exerciseIds: exIds}
+  }),{
+    onSuccess(){
+      setName('')
+      setCurrentExercise([])
+    }
+  })
+
+  const options: {value:string,label:string}[] = exercisesId.map((item:any) => ({value:item._id,label:item.name}))
  
-  const handleSubmit = () => {
-    console.log('submit')
+  const handleSubmit = (e:any) => {
+    e.preventDefault()
+    const exIds = currentExercise.map((item:any) => item.value)
+    mutate({
+      exIds
+    })
+  }
+
+  const handleExercise = (e:any) => {
+    setCurrentExercise(e)
   }
 
   const nameHandler = (e: any) => {
@@ -29,9 +62,23 @@ const NewWorkout = ():JSX.Element => {
             Create new Workout
           </Title>
         </div>
+          <>
+            {error && <Alert type="error" text={error}/>}
+          </>
+          <>
+            {postSuccess && <Alert type="success" text="Workoute create success"/>}
+          </>
         <form onSubmit={handleSubmit} className="newWorkout__form">
           <Field placeHolder='Name' type='text' value={name} changeValue={nameHandler}/>
-          <Select classNamePrefix='newWorkout__select' placeholder='Exercises' isMulti={true} value={exercises} onChange={() => console.log(1)} options={options}/>
+          {isSuccess && exercisesId && 
+            <Select 
+              classNamePrefix='newWorkout__select' 
+              placeholder='Exercises' 
+              isMulti={true} 
+              value={currentExercise}
+              onChange={(e) => handleExercise(e)} 
+              options={options}
+            />}
           <Button onClick={() => console.log('Действие')}>
             Create
           </Button>
